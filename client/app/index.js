c0076fc4d3e60a03476367d029cdd6c932f4b871
Reshav Abraham <reshavabraham@gmail.com>
2021-04-05 17:07:32 -0400

    card dashboard

--- client/app/index.js
+++ client/app/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Button from '@material-ui/core/Button';
+import PlannerCards from './components/plannerCards';
+
+// check for existing cards
+
 
 function App() {
   return (
+    <>
+    <PlannerCards />
     <Button onClick={()=>{console.log("What's good!")}} variant="contained" color="primary">
       Hello World
     </Button>
+    </>
   );
 }
 
