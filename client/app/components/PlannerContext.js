5af2cae8fd9953079688b3cbfc139bfffba0166e
Reshav Abraham <reshavabraham@gmail.com>
2021-04-15 10:39:23 -0400

    usecontext and state

--- client/app/components/PlannerContext.js
+++ client/app/components/PlannerContext.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
-export const PlannerContext = React.createContext();
+export const PlannerContext = React.createContext({'selectedPlan':''});
 
-function countReducer(state, action) {
-    switch (action.type) {
-      case 'increment': {
-        return {count: state.count + 1}
-      }
-      case 'decrement': {
-        return {count: state.count - 1}
-      }
-      default: {
-        throw new Error(`Unhandled action type: ${action.type}`)
-      }
+export default ({ children }) => {
+    const teamMembersNames = ['John', 'Mary', 'Jason', 'David']
+  
+    const [sharing, setSharing] = React.useState([])
+    const [help, setHelp] = React.useState([])
+    const [pairing, setPairing] = React.useState(teamMembersNames)
+  
+    const store = {
+      sharing: [sharing, setSharing],
+      help: [help, setHelp],
+      pairing: [pairing, setPairing],
     }
+  
+    return <PlannerContext.Provider value={store}>{children}</PlannerContext.Provider>
   }
-
-function PlannerProvider({children}) {
-    const [state, dispatch] = React.useReducer(countReducer, {count: 0})
-    // NOTE: you *might* need to memoize this value
-    // Learn more in http://kcd.im/optimize-context
-    const value = {state, dispatch}
-    return <PlannerContext.Provider value={value}>{children}</PlannerContext.Provider>
-}
-
-export default PlannerProvider;
