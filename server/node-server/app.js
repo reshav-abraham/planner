eb6ae72c849bc44ddbf5a61f9cfca9144c0dfa93
Reshav Abraham <reshavabraham@gmail.com>
2021-04-25 00:49:32 -0400

    node server cors

--- server/node-server/app.js
+++ server/node-server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var cors = require('cors');
 const db = require('./db');
-const app = express()
-const port = 5000
+const app = express();
+app.use(cors());
+app.set('port', process.env.PORT || 3000);
+const port = 8000;
 
 app.get('/', async (req, res, next) => {
   try {
@@ -12,6 +15,30 @@ app.get('/', async (req, res, next) => {
     res.sendStatus(500);
   }
 });
+
+app.post('/createUser', async (req, res, next) => {
+  console.log('create User');
+  // try {
+  //   let results = await db.all();
+  //   res.json(results);
+  // } catch(e){
+  //   console.log(e);
+  //   res.sendStatus(500);
+  // }
+});
+
+app.post('/createPlan', async (req, res, next) => {
+  console.log('create User');
+  res.json({"message":"success"});
+  // try {
+  //   let results = await db.all();
+  //   res.json(results);
+  // } catch(e){
+  //   console.log(e);
+  //   res.sendStatus(500);
+  // }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
